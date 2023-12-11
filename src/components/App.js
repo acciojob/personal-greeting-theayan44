2b881236838da66bf96e00e0d8d42b36f34eeb0c
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-12-11 13:47:28 +0000

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [message, setMessage] = useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text"  onKeyUp={(event)=>setMessage(`Hello ${event.target.value}!`)}/>
+        <p>{message}</p>
     </div>
   )
 }
